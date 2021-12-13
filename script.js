431983e82ae57e645da87f8c3dc4f0be7e5419fc
Zeinab Farag <zeinabfarag454@gmail.com>
2021-12-13 16:21:05 -0500

    showing time left

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 const form = document.getElementById("countdownForm");
 const inputContainer = document.getElementById("input-container");
-const titleInput = document.getElementById("title");
 const dateEl = document.getElementById("date-picker");
+
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
 
 const today = new Date().toISOString().split("T")[0];
 
 dateEl.setAttribute("min", today);
-const formSubmit = (e) => {
+
+let countdownTitle = "";
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+const updateCountdown = (e) => {
   e.preventDefault();
+
+  countdownTitle = e.srcElement[0].value;
+  let countdownDate = e.srcElement[1].value;
+
+  countdownValue = new Date(countdownDate).getTime();
+  updateDom();
 };
-form.addEventListener("submit", formSubmit);
+
+const updateDom = () => {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+
+  countdownElTitle.textContent = `${countdownTitle}`;
+
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  inputContainer.hidden = true;
+
+  countdownEl.hidden = false;
+};
+form.addEventListener("submit", updateCountdown);
